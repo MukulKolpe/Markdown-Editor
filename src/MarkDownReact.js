e43c97e14d4be20e2df4d8d176f5d7a9a240e3b3
Mukul <mukulkolpe45@gmail.com>
2021-08-05 01:41:50 +0530

    initial commit

---
+++ src/MarkDownReact.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import ReactMarkdown from 'react-markdown'
+import './style.css'
+
+ const MarkDownReact = () => {
+     const [markDown, setMarkDown]=useState("This is markdown");
+    return <>
+        <div className="center-div">
+            <textarea className="left-side" value={markDown} onChange={(e)=> setMarkDown(e.target.value)}></textarea>
+            <div className="right-side">
+                <ReactMarkdown>{markDown}</ReactMarkdown></div>
+        </div>
+    </>;
+}
+export default MarkDownReact;
